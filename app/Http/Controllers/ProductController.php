9ad80b801a377daa15447ae1db0259f8b6786154
llacuna750 <llacuna750@gmail.com>
2026-04-04 15:50:16 +0800

    DdsGW__  Add ProductController, ProxyController & Product Model

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Product::all());
+    }
+
+    public function show($id)
+    {
+        return response()->json(Product::findOrFail($id));
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'required|numeric',
+            'stock' => 'required|integer',
+        ]);
+
+        return response()->json(Product::create($data), 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::findOrFail($id);
+        $data = $request->validate([
+            'name' => 'sometimes|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'sometimes|numeric',
+            'stock' => 'sometimes|integer',
+        ]);
+
+        $product->update($data);
+        return response()->json($product);
+    }
+
+    public function destroy($id)
+    {
+        Product::findOrFail($id)->delete();
+        return response()->json(['message' => 'Deleted']);
+    }
+}
---
+++ app/Http/Controllers/ProxyController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Http;
+
+class ProxyController extends Controller
+{
+    private $site2Url;
+    private $site2Secret;
+
+    public function __construct()
+    {
+        $this->site2Url = env('USERS2_SERVICE_BASE_URL');
+        $this->site2Secret = env('USERS2_SERVICE_SECRET');
+    }
+
+    public function getProducts()
+    {
+        $response = Http::withHeaders([
+            'Authorization' => 'Bearer ' . $this->site2Secret,
+        ])->get($this->site2Url . '/products');
+
+        return response()->json($response->json(), $response->status());
+    }
+
+    public function showProduct($id)
+    {
+        $response = Http::withHeaders([
+            'Authorization' => 'Bearer ' . $this->site2Secret,
+        ])->get($this->site2Url . '/products/' . $id);
+
+        return response()->json($response->json(), $response->status());
+    }
+
+    public function storeProduct(\Illuminate\Http\Request $request)
+    {
+        $response = Http::withHeaders([
+            'Authorization' => 'Bearer ' . $this->site2Secret,
+        ])->post($this->site2Url . '/products', $request->all());
+
+        return response()->json($response->json(), $response->status());
+    }
+
+    public function updateProduct(\Illuminate\Http\Request $request, $id)
+    {
+        $response = Http::withHeaders([
+            'Authorization' => 'Bearer ' . $this->site2Secret,
+        ])->put($this->site2Url . '/products/' . $id, $request->all());
+
+        return response()->json($response->json(), $response->status());
+    }
+
+    public function destroyProduct($id)
+    {
+        $response = Http::withHeaders([
+            'Authorization' => 'Bearer ' . $this->site2Secret,
+        ])->delete($this->site2Url . '/products/' . $id);
+
+        return response()->json($response->json(), $response->status());
+    }
+}
