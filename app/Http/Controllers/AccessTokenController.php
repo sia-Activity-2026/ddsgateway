cad7b3f29ba2e88bb4c94118be378aee8cbfaf9b
llacuna750 <llacuna750@gmail.com>
2026-04-10 17:43:56 +0800

    ddsGW_ re-Migrate & Update AccessTokenController.php

--- app/Http/Controllers/AccessTokenController.php
+++ app/Http/Controllers/AccessTokenController.php
@@ -3,7 +3,20 @@
 namespace App\Http\Controllers;
 
 use Dusterio\LumenPassport\Http\Controllers\AccessTokenController as LumenPassportAccessTokenController;
+use Laravel\Passport\TokenRepository;
+use League\OAuth2\Server\AuthorizationServer;
+use Lcobucci\JWT\Parser as JwtParser;
 
+/**
+ * Passport 11 removed $jwt from the base AccessTokenController; lumen-passport still expects it.
+ */
 class AccessTokenController extends LumenPassportAccessTokenController
 {
+    protected JwtParser $jwt;
+
+    public function __construct(AuthorizationServer $server, TokenRepository $tokens, JwtParser $jwt)
+    {
+        parent::__construct($server, $tokens);
+        $this->jwt = $jwt;
+    }
 }
