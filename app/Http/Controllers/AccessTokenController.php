2e273630ee0bac3ff915b22b45db0b008e796f66
llacuna750 <llacuna750@gmail.com>
2026-04-10 17:54:30 +0800

    ddsGW_ can't recieve token

--- app/Http/Controllers/AccessTokenController.php
+++ app/Http/Controllers/AccessTokenController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers;
 
 use Dusterio\LumenPassport\Http\Controllers\AccessTokenController as LumenPassportAccessTokenController;
+use Lcobucci\JWT\Configuration;
+use Lcobucci\JWT\Token\Parser as JwtParser;
 use Laravel\Passport\TokenRepository;
 use League\OAuth2\Server\AuthorizationServer;
-use Lcobucci\JWT\Parser as JwtParser;
 
 /**
  * Passport 11 removed $jwt from the base AccessTokenController; lumen-passport still expects it.
@@ -14,9 +15,9 @@ class AccessTokenController extends LumenPassportAccessTokenController
 {
     protected JwtParser $jwt;
 
-    public function __construct(AuthorizationServer $server, TokenRepository $tokens, JwtParser $jwt)
+    public function __construct(AuthorizationServer $server, TokenRepository $tokens)
     {
         parent::__construct($server, $tokens);
-        $this->jwt = $jwt;
+        $this->jwt = Configuration::forUnsecuredSigner()->parser();
     }
 }
